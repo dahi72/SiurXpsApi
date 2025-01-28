bd64e26da3bbe904d56dec38eda143d0eea394eb
dahi72 <castelli.dahiana@gmail.com>
2025-01-28 20:01:51 -0300

    Update HotelController.cs

--- WebApp/Controllers/HotelController.cs
+++ WebApp/Controllers/HotelController.cs
@@ -16,9 +16,9 @@ namespace WebApp.Controllers
         private readonly IBajaHotel CUBaja;
         private readonly IFiltroHotelesPorPaisYCiudad CUFiltro;
 
-        public HotelController(IAltaHotel cuAlta, IBuscarHotelPorId cuBuscar,  IModificarHotel cuModificar, IBajaHotel cuBajaHotel, IFiltroHotelesPorPaisYCiudad cuFiltro) 
-        
-        {  
+        public HotelController(IAltaHotel cuAlta, IBuscarHotelPorId cuBuscar, IModificarHotel cuModificar, IBajaHotel cuBajaHotel, IFiltroHotelesPorPaisYCiudad cuFiltro)
+
+        {
             CUAltaHotel = cuAlta;
             CUBuscar = cuBuscar;
             CUModificar = cuModificar;
@@ -28,7 +28,7 @@ namespace WebApp.Controllers
 
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-       // [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        // [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
 
         [HttpPost("altaHotel")]
@@ -41,7 +41,7 @@ namespace WebApp.Controllers
                 CUAltaHotel.Alta(hotel);
                 return Ok(new { codigo = 200, mensaje = "Hotel creado exitosamente." });
             }
-          
+
             catch (InvalidOperationException ex)
             {
                 return BadRequest(new { message = ex.Message });
@@ -65,10 +65,10 @@ namespace WebApp.Controllers
 
         public IActionResult Put(int id, [FromForm] HotelDTO hotelDto)
         {
-           
+
             try
             {
-               
+
                 hotelDto.Id = id;
                 CUModificar.Modificar(hotelDto);
 
@@ -106,12 +106,12 @@ namespace WebApp.Controllers
 
             try
             {
-           
+
                 HotelDTO hotel = CUBuscar.Buscar(id);
                 if (hotel == null)
                     return NotFound($"No existe el hotel con el id {id}");
 
-               
+
                 CUBaja.Eliminar(hotel);
 
                 return NoContent();
@@ -167,4 +167,14 @@ namespace WebApp.Controllers
 
         }
     }
-}
+  
+   
+        [HttpGet("saludo")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult ObtenerSaludo()
+        {
+            return Ok("Hola Mundo");
+        }
+
+
+    }
